97a8e56ff6921613e19fd4d8a981a5bdfa476619
Uri <oriol.castroarnau@gmail.com>
2018-09-21 10:23:38 +0200

    Added new links

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -28,22 +28,52 @@ const Profile = ({ isBlogPost }) => {
             <Header size="tiny" style={{ textAlign: "justify" }}>
               Sóc programador web front-end, emprenedor digital i entusiasta
               tecnològic. En aquest blog escric sobre tecnologia i canvi social.
-              Treballo a <a href="https://okstudio.tech">OK! Studio</a>.
+              Treballo a{" "}
+              <a
+                href="https://okstudio.tech"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                OK! Studio
+              </a>
+              .
             </Header>
           </>
         )}
         <div>
-          <a href="https://twitter.com/oriolcastro_">
+          <a
+            href="https://twitter.com/oriolcastro_"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
             <Icon name="twitter" circular link />
           </a>
-          <a href="https://www.instagram.com/oriolcastro_/">
+          <a
+            href="https://www.instagram.com/oriolcastro_/"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
             <Icon name="instagram" circular link />
           </a>
-          <a href="https://www.linkedin.com/in/oriolcaar/">
+          <a
+            href="https://www.linkedin.com/in/oriolcaar/"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
             <Icon name="linkedin" circular link />
           </a>
-          <a href="https://github.com/oriolcastro">
+          <a
+            href="https://github.com/oriolcastro"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
             <Icon name="github" circular link />
+          </a>
+          <a
+            href="mailto:uri@oriolcastro.me"
+            rel="nofollow noopener noreferrer"
+          >
+            <Icon name="envelope" circular link />
           </a>
         </div>
       </Grid.Column>

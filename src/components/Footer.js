5db96f68222604fb7586825f92a455584b4148a7
Uri <oriol.castroarnau@gmail.com>
2018-09-21 10:23:54 +0200

    Added link to tech used

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,42 @@ const Footer = () => (
       borderTop: "solid 1px gray "
     }}
   >
-    Made with <Icon name="heart" color="red" /> in Vilanova
+    Made with <Icon name="heart" color="red" /> in Vilanova.
+    <br />
+    <Icon name="code" color="blue" /> using{" "}
+    <a
+      href="https://www.gatsbyjs.org/"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      Gatsby
+    </a>{" "}
+    and{" "}
+    <a
+      href="https://reactjs.org/"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      React
+    </a>
+    , styled with{" "}
+    <a
+      href="https://semantic-ui.com/"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      {" "}
+      Semantic-UI
+    </a>{" "}
+    and delivered by{" "}
+    <a
+      href="https://www.netlify.com/"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      Netlify
+    </a>
+    .
   </Container>
 );
 

c97cecfd5214e556eb3bcd2de82cbcd18229e0a4
Uri <oriol.castroarnau@gmail.com>
2019-02-07 12:35:26 +0100

    Final version of Portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { graphql } from "gatsby";
-import { Header, Container, Card, Button, Label } from "semantic-ui-react";
-import { IconContext } from "react-icons";
-import { FaExternalLinkSquareAlt, FaCode } from "react-icons/fa";
+import { graphql, Link } from "gatsby";
+import { Header, Container } from "semantic-ui-react";
 import Img from "gatsby-image";
+import { OutboundLink } from "gatsby-plugin-gtag";
 
 import Layout from "../components/layout";
+import GithubCards from "../components/GithubCards";
+import CVButtons from "../components/CVButtons";
 
 const PortfolioPage = props => {
   const { edges: repos } = props.data.github.user.repositories;
@@ -17,71 +18,35 @@ const PortfolioPage = props => {
       <Header as="h1" style={{ marginBottom: "24px" }}>
         Portfoli
       </Header>
-      <Container>
-        <Card.Group itemsPerRow="2" stackable>
-          {repos.map(({ node: repo }) => (
-            <Card key={repo.id}>
-              <Card.Content>
-                <Card.Header style={{ textTransform: "capitalize" }}>
-                  {repo.name}
-                </Card.Header>
-                <Card.Description>
-                  <p style={{ marginBottom: "16px", height: "6rem" }}>
-                    {repo.description}
-                  </p>
-                  <Button
-                    basic
-                    color="blue"
-                    href={repo.homepageUrl}
-                    target="_blank"
-                    rel="nofollow noopener noreferrer"
-                    style={{ marginRight: "8px" }}
-                  >
-                    <IconContext.Provider
-                      value={{
-                        style: {
-                          verticalAlign: "text-top",
-                          marginRight: "8px"
-                        },
-                        size: "1.15em"
-                      }}
-                    >
-                      <FaExternalLinkSquareAlt />
-                    </IconContext.Provider>
-                    Projecte
-                  </Button>
-                  <Button
-                    secondary
-                    href={repo.url}
-                    target="_blank"
-                    rel="nofollow noopener noreferrer"
-                  >
-                    <IconContext.Provider
-                      value={{
-                        style: {
-                          verticalAlign: "text-top",
-                          marginRight: "8px"
-                        },
-                        size: "1.15em"
-                      }}
-                    >
-                      <FaCode />
-                    </IconContext.Provider>
-                    Codi
-                  </Button>
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                {repo.repositoryTopics.edges.map(({ node: t }) => (
-                  <Label key={t.id} style={{ marginBottom: "8px" }}>
-                    {t.topic.name}
-                  </Label>
-                ))}
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
+      <Container
+        textAlign="justified"
+        content
+        text
+        style={{ marginBottom: "24px", whiteSpace: "pre-line" }}
+      >
+        <p>
+          Aquí podeu consultar els últims projectes en que he treballat ja sigui
+          pel meu compte o com a desenvolupador web freelance per a diferents
+          clients.{"\n"}
+          Tot el codi està allotjat al meu compte de Github i és consultable.
+          També podeu veure el resultat final de cada projecte.{"\n"}
+          Si voleu més informció podeu llegir els articles del meu{" "}
+          <Link to="/blog">blog</Link> o els apartats de{" "}
+          <OutboundLink
+            href="https://www.okstudio.tech/projectes"
+            target="_blank"
+          >
+            projectes
+          </OutboundLink>{" "}
+          i{" "}
+          <OutboundLink href="https://www.okstudio.tech/lab" target="_blank">
+            el LAB
+          </OutboundLink>{" "}
+          de la web d'Ok! Studio.
+        </p>
       </Container>
+      <GithubCards repos={repos} />
+      <CVButtons />
     </Layout>
   );
 };
@@ -92,7 +57,7 @@ export const query = graphql`
   query PortfolioPageQuery {
     file(name: { eq: "portfoliHero" }) {
       childImageSharp {
-        fluid(maxWidth: 700, maxHeight: 300) {
+        fluid(maxWidth: 1400, maxHeight: 700) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }

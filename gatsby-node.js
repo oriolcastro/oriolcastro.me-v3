002737b6dc7a49242ddd80e6f5da0347e4ae041a
Uri <oriol.castroarnau@gmail.com>
2020-03-07 19:23:47 +0100

    refactor: how pages are generated with new structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,122 +1,121 @@
-const _ = require("lodash");
+const get = require("lodash/get");
+const uniq = require("lodash/uniq");
+const kebabCase = require("lodash/kebabCase");
 const graphql = require("gatsby");
 const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.createPages = ({ actions, graphql }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
-  const loadPages = new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark(
-          filter: { frontmatter: { templateKey: { eq: "single-page" } } }
-        ) {
-          edges {
-            node {
-              id
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-    `).then(result => {
-      if (result.errors) {
-        result.errors.forEach(e => console.error(e.toString()));
-        return Promise.reject(result.errors);
-      }
-      const pages = result.data.allMarkdownRemark.edges;
-      pages.forEach((edge, i) => {
-        const id = edge.node.id;
-        createPage({
-          path: edge.node.fields.slug,
-          component: path.resolve(`src/templates/single-page.js`),
-          context: {
+  const singlePageTemplate = path.resolve(`src/templates/single-page.js`);
+  const singlePageData = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { frontmatter: { templateKey: { eq: "single-page" } } }
+      ) {
+        edges {
+          node {
             id
-          }
-        });
-      });
-      resolve();
-    });
-  });
-
-  const loadPosts = new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark(
-          limit: 1000
-          sort: { order: ASC, fields: frontmatter___date }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-        ) {
-          edges {
-            node {
-              id
-              fields {
-                slug
-              }
-              frontmatter {
-                tags
-              }
+            fields {
+              slug
             }
           }
         }
       }
-    `).then(result => {
-      if (result.errors) {
-        result.errors.forEach(e => console.error(e.toString()));
-        return Promise.reject(result.errors);
+    }
+  `);
+
+  if (singlePageData.errors) {
+    result.errors.forEach(e => console.error(e.toString()));
+    throw result.errors;
+  }
+  // Create single pages
+  const pages = singlePageData.data.allMarkdownRemark.edges;
+  pages.forEach((page, i) => {
+    createPage({
+      path: page.node.fields.slug,
+      component: singlePageTemplate,
+      context: {
+        id: page.node.id
       }
-
-      const posts = result.data.allMarkdownRemark.edges;
-
-      posts.forEach((edge, i) => {
-        const id = edge.node.id;
-        const prev = i === 0 ? null : posts[i - 1].node;
-        const next = i === posts.length - 1 ? null : posts[i + 1].node;
-        const slug = edge.node.fields.slug;
-        createPage({
-          path: edge.node.fields.slug,
-          tags: edge.node.frontmatter.tags,
-          component: path.resolve(`src/templates/blog-post.js`),
-          // additional data can be passed via context
-          context: {
-            id,
-            prev,
-            next,
-            slug
-          }
-        });
-      });
-
-      // Tag pages:
-      let tags = [];
-      // Iterate through each post, putting all found tags into `tags`
-      posts.forEach(edge => {
-        if (_.get(edge, `node.frontmatter.tags`)) {
-          tags = tags.concat(edge.node.frontmatter.tags);
-        }
-      });
-      // Eliminate duplicate tags
-      tags = _.uniq(tags);
-
-      // Make tag pages
-      tags.forEach(tag => {
-        const tagPath = `/tags/${_.kebabCase(tag)}/`;
-
-        createPage({
-          path: tagPath,
-          component: path.resolve(`src/templates/tags.js`),
-          context: {
-            tag
-          }
-        });
-      });
-      resolve();
     });
   });
-  return Promise.all([loadPages, loadPosts]);
+
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+  const blogData = await graphql(`
+    {
+      allMarkdownRemark(
+        limit: 1000
+        sort: { order: ASC, fields: frontmatter___date }
+        filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      ) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              tags
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (blogData.errors) {
+    result.errors.forEach(e => console.error(e.toString()));
+    throw result.error;
+  }
+
+  // Create blog post pages
+  const posts = blogData.data.allMarkdownRemark.edges;
+
+  posts.forEach((post, i) => {
+    const id = post.node.id;
+    const prev = i === 0 ? null : posts[i - 1].node;
+    const next = i === posts.length - 1 ? null : posts[i + 1].node;
+
+    createPage({
+      path: post.node.fields.slug,
+      tags: post.node.frontmatter.tags,
+      component: blogPostTemplate,
+      // additional data can be passed via context
+      context: {
+        id,
+        prev,
+        next,
+        slug: post.node.fields.slug
+      }
+    });
+  });
+
+  // Tag pages:
+  let tags = [];
+  const tagsPageTemplate = path.resolve(`src/templates/tags.js`);
+  // Iterate through each post, putting all found tags into `tags`
+  posts.forEach(post => {
+    if (get(post, `node.frontmatter.tags`)) {
+      tags = tags.concat(post.node.frontmatter.tags);
+    }
+  });
+  // Eliminate duplicate tags
+  tags = uniq(tags);
+
+  // Make tag pages
+  tags.forEach(tag => {
+    const tagPath = `/tags/${kebabCase(tag)}/`;
+
+    createPage({
+      path: tagPath,
+      component: tagsPageTemplate,
+      context: {
+        tag
+      }
+    });
+  });
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -129,5 +128,19 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       node,
       value
     });
+
+    if (node.frontmatter.templateKey === "blog-post") {
+      const relativeFilePath = createFilePath({
+        node,
+        getNode,
+        basePath: "content/blog/",
+        trailingSlash: false
+      });
+      createNodeField({
+        name: `slug`,
+        node,
+        value: `/blog${relativeFilePath}`
+      });
+    }
   }
 };
